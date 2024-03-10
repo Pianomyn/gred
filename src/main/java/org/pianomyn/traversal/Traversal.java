cc7c7427c40ffe9122c2f165e4f923282ee2e1f2
Pianomyn <anditan2014@gmail.com>
2024-03-11 09:03:21 +1100

    Traversal using BFS. Still needs tests

--- src/main/java/org/pianomyn/traversal/Traversal.java
+++ src/main/java/org/pianomyn/traversal/Traversal.java
@@ -1,44 +1,66 @@
 package org.pianomyn.traversal;
 
 import java.io.File;
-import java.util.ArrayDeque;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Traversal {
-  private Queue<String> files;
-  private String root;
+    private Queue<String> queue;
+    private Queue<String> result;
+    private String root;
 
-  public Traversal(String root, Queue<String> files) {
-    this.root = root;
-    this.files = files;
-  }
+    public Traversal(String root) {
+        this.root = root;
+        this.queue = new LinkedList<>();
+        this.result = new LinkedList<>();
+    }
 
-  public String getRoot() { return this.root; }
-  public Queue<String> getFiles() { return this.files; }
+    // Getters
+    public String getRoot() { return this.root; }
+    public Queue<String> getResult() { return this.result; }
 
-  public void traverse() {
-    File parent = new File(this.root);
-    File[] children = parent.listFiles();
+    // Setters
+    public void setRoot(String newRoot) { this.root = newRoot; }
 
-    if (files != null) {
-      for (File c : children) {
-        if (c.isDirectory()) {
-          traverse();
-        } else {
-          this.files.add(c.getName());
+    // Instance methods
+    public void traverse(String root) {
+        this.result.clear();
+        File currentFile = new File(this.root);
+        if(currentFile.exists()) { this.queue.offer(this.root); }
+
+        while(!this.queue.isEmpty()) {
+            this.exploreCurrentDepth();
         }
-      }
     }
-  }
 
-  public static void main(String[] args) {
-    System.out.println("Hello world");
-    Traversal t =
-        new Traversal("~/testtraversaldir/", new LinkedList<String>());
-    t.traverse();
-    while(t.getFiles().size() > 0) {
-      System.out.println(t.getFiles().poll());
+    private void exploreCurrentDepth() {
+        int n = this.queue.size();
+        String currentName;
+        File currentFile;
+        while(n > 0) {
+            currentName = this.queue.poll();
+            this.enqueueChildren(currentName);
+            n--;
+        }
     }
-  }
+
+    private void enqueueChildren(String currentName) {
+        File currentFile = new File(currentName);
+        if(currentFile.isDirectory()) {
+            for(File child : currentFile.listFiles()) {
+                this.queue.offer(currentName + "/" + child.getName());
+            }
+        } else if(currentFile.isFile()) {
+            this.result.offer(currentName);
+        }
+    }
+
+    public static void main(String[] args) {
+    Traversal t = new Traversal("/home/andi/mydir");
+    System.out.println("Started");
+    t.traverse(t.getRoot());
+    while (!t.getResult().isEmpty()) {
+        System.out.println(t.getResult().poll());
+    }
+        System.out.println("Ended");
+    }
 }
