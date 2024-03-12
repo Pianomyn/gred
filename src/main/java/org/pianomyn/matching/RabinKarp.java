1e138ce7ad6feac82683c7fba0641a6a8402ab92
Pianomyn <anditan2014@gmail.com>
2024-03-12 23:33:07 +1100

    removes commented code

--- src/main/java/org/pianomyn/matching/RabinKarp.java
+++ src/main/java/org/pianomyn/matching/RabinKarp.java
@@ -6,8 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RabinKarp extends MatchingAlgorithm {
-    //private String filePath;
-    //private String pattern;
     private int BASE = 256;  // Size of alphabet
     private int PRIME = 101;  // Prime number close to size of alphabet (ASCII-256 in this instance).
 
@@ -36,7 +34,6 @@ public class RabinKarp extends MatchingAlgorithm {
             String line;
             int lineNumber = 1;
             while ((line = br.readLine()) != null) {
-                // Rabin Karp here;
                 // TODO: Case where line shorter than pattern?
                 int m = this.pattern.length();
                 int n = line.length();
