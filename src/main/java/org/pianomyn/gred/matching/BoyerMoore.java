5749a14a04e6e4803fd0d68ea13a828228e18f38
Pianomyn <anditan@skiff.com>
2024-03-31 20:11:14 +1100

    skeleton and pseudocode for boyer moore

--- src/main/java/org/pianomyn/gred/matching/BoyerMoore.java
+++ src/main/java/org/pianomyn/gred/matching/BoyerMoore.java
@@ -1,3 +1,31 @@
 package org.pianomyn.gred.matching;
 
-public class BoyerMoore {}
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoyerMoore extends MatchingAlgorithm {
+  public BoyerMoore(Path filePath, String pattern) {
+    super(filePath, pattern);
+  }
+
+  @Override
+  public List<List<Integer>> findMatches() {
+    /*
+     * Bad char table
+     *   Should be matrix (all chars in alphabet vs all chars in pattern)
+     * TODO: Good suffix table
+     *
+     * Line up pattern with left side of text
+     * Iterate from right to left
+     *  If match, advance pattern by 1 (as pattern could overlap with itself)
+     *  Else (Idea is to shift pattern right to make the mismatch a match)
+     *    If mismatching character doesnt exist in pattern, shift by length of pattern
+     *    If exists, shift to rightmost occurrence to the left of the mismatch
+     *    For both, consult bad char table
+     */
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    return result;
+  }
+}
+
