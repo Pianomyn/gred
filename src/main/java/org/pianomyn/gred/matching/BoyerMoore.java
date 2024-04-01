e4acb5dd2b72e1b2c36de90710488d66c8d41717
Pianomyn <anditan@skiff.com>
2024-04-02 00:18:25 +1100

    pseudocode update

--- src/main/java/org/pianomyn/gred/matching/BoyerMoore.java
+++ src/main/java/org/pianomyn/gred/matching/BoyerMoore.java
@@ -10,9 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 public class BoyerMoore extends MatchingAlgorithm {
-  public BoyerMoore(Path filePath, String pattern) {
-    super(filePath, pattern);
-  }
+  public BoyerMoore(Path filePath, String pattern) { super(filePath, pattern); }
 
   @Override
   public List<List<Integer>> findMatches() {
@@ -25,9 +23,9 @@ public class BoyerMoore extends MatchingAlgorithm {
      * Iterate from right to left
      *  If match, advance pattern by 1 (as pattern could overlap with itself)
      *  Else (Idea is to shift pattern right to make the mismatch a match)
-     *    If mismatching character doesnt exist in pattern, shift by length of pattern
-     *    If exists, shift to rightmost occurrence to the left of the mismatch
-     *    For both, consult bad char table
+     *    If mismatching character doesnt exist in pattern, shift by length of
+     * remaining pattern If exists, shift to rightmost occurrence to the left of
+     * the mismatch For both, consult bad char table
      */
     List<List<Integer>> result = new ArrayList<List<Integer>>();
     if (pattern == null || !this.fileExists()) {
@@ -45,7 +43,7 @@ public class BoyerMoore extends MatchingAlgorithm {
       while ((line = br.readLine()) != null) {
         // Logic here
       }
-    } catch(IOException e) {
+    } catch (IOException e) {
       e.printStackTrace();
     }
     return result;
@@ -59,7 +57,7 @@ public class BoyerMoore extends MatchingAlgorithm {
     for (int r = 0; r < 128; r++) {
       lastSeenIndex.clear();
       for (int c = 0; c < patternLength; c++) {
-        char currentChar = (char) r;
+        char currentChar = (char)r;
 
         if (this.pattern.charAt(c) == currentChar) {
           badCharTable[r][c] = 0;
