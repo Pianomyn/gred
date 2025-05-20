1ded6756e47ffd1493804b332e2a7ec0bb0b6a62
Pianomyn <anditan2014@gmail.com>
2025-05-21 03:01:22 +1000

    Comment on poison pill

--- src/main/java/org/pianomyn/gred/orchestration/PoisonPill.java
+++ src/main/java/org/pianomyn/gred/orchestration/PoisonPill.java
@@ -3,6 +3,6 @@ package org.pianomyn.gred.orchestration;
 import java.nio.file.Path;
 
 public class PoisonPill {
-  // Gravestones to allow consumers to gracefully exit.
+  // Marker to allow consumers to gracefully exit.
   public static final Path POISON_PILL = Path.of("__POISON_PILL__");
 }
